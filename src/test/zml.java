668d1fb65a2d404db1a930faeda25c5372f05742
zml <zhaoml529@163.com>
2016-03-09 14:41:06 +0800

    功能分支feature

--- src/test/zml.java
+++ src/test/zml.java
@@ -2,8 +2,14 @@ package test;
 
 public class zml {
 
+	private static void feature() {
+		System.out.println("这个方法实在功能性分支(feature)上开发的！");
+		System.out.println("开发者在本地仓库从develop分支分出功能分支，在该分支上进行功能的开发，开发完成以后再合并到develop分支上，这时候功能性分支已经完成任务，可以删除。");
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("��֧����");
+		System.out.println("aa");
 		System.out.println("wsq");
+		feature();
 	}
 }
